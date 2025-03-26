ab7d5d5d6cb1785867299b74908d5bd13ee2352d
Janel Bañaria <janel.banaria@Gmail.com>
2025-03-25 21:10:19 -0600

    Update joke.js
    add programming joke

--- joke.js
+++ joke.js
@@ -6,3 +6,16 @@ fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
     console.log(data);
     RandomJoke.textContent = data.joke || data.setup +" ... "+ data.delivery;
 });
+
+//Programming
+
+document.getElementById('programming').addEventListener('click', () => {
+    fetch('https://v2.jokeapi.dev/joke/Programming?safe-mode')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        document.getElementById('RandomJoke').textContent = data.joke || data.setup +" ... "+ data.delivery;
+    })
+
+    document.getElementById('PageTitle').textContent = "A Programming Joke!"; 
+})
